f3d45413b34868931febf3a6d07a0ca2bf91a04e
Yannick <yannick.boogaerts@technifutur.be>
2021-04-21 14:08:24 +0200

    petit test de Sudoku4x4

--- src/be/technifutur/sudoku/Main.java
+++ src/be/technifutur/sudoku/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Sudoku");
+        TestSudoku4x4();
+    }
+
+    private static void TestSudoku4x4() {
+        Sudoku4X4 sudoku4X4 = new Sudoku4X4();
+        System.out.println(sudoku4X4.isCellValid(0,0)?"ok":"ko");
     }
 
 }
